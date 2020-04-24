e0a3f2e8d03db36d9c5cbc6f692f3bf08f007984
KimChanghyun72 <ok2qkq@naver.com>
2020-04-24 13:33:28 +0900

    fourth

--- IntExample.java
+++ IntExample.java
@@ -8,4 +8,9 @@ public class IntExample {
 		System.out.print("num1: "+num1+"\t num2: " +num2+"\t num3: "+num3 + "\n" );
 		System.out.println("end of program");
 	}
+	if (){
+	System.out.println("Master Yondu..:);
+	} else {
+		System.out.println("Power! Unlimited Power!!!");
+	}
 }
