4f79ed052d1349c1e708b44d68bc0268fc3079a8
KimChanghyun72 <ok2qkq@naver.com>
2020-04-27 12:37:02 +0900

    2020_04_27

---
+++ StringConcatExample.java
@@ -0,0 +1,16 @@
+
+public class StringConcatExample {
+    public static void main(String[] args) {
+        System.out.println("Hello, World! mama I dont wanna say hello this fuckin world" );
+        String str1 = "Hello";
+        String str2 = "World";
+        String str3 = str1 + ", " + str2;
+        System.out.println(str3);
+
+        System.out.println("result: "+ 3 + 30);
+        System.out.println("result: "+ (3 + 30)); //근데 이럴바엔 변수하나 선언해주거나 하지 굳이?//
+        System.out.println(3+30+"result: ");
+    
+    }
+
+}
